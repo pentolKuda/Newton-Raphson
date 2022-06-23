ab02a76a05d395bbad104dfcf52ab2744f39a57f
F_Zamzami <68177295+pentolKuda@users.noreply.github.com>
2022-06-23 17:53:07 +0800

    first commit

---
+++ Newton-Rhapson/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace Newton_Rhapson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupEquation = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInputNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupEquation.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(38, 329);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 230);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Guess Number";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(30, 38);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(380, 113);
+            this.dataGridView2.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Solve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupEquation
+            // 
+            this.groupEquation.Controls.Add(this.dataGridView1);
+            this.groupEquation.Controls.Add(this.label6);
+            this.groupEquation.Controls.Add(this.label5);
+            this.groupEquation.Location = new System.Drawing.Point(38, 172);
+            this.groupEquation.Name = "groupEquation";
+            this.groupEquation.Size = new System.Drawing.Size(482, 142);
+            this.groupEquation.TabIndex = 23;
+            this.groupEquation.TabStop = false;
+            this.groupEquation.Text = "Equation";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(380, 60);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Degree Equation";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbInputNumber);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(38, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(482, 118);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equation Setup";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Degree of  Equation Number";
+            // 
+            // tbInputNumber
+            // 
+            this.tbInputNumber.Location = new System.Drawing.Point(302, 40);
+            this.tbInputNumber.Name = "tbInputNumber";
+            this.tbInputNumber.Size = new System.Drawing.Size(100, 22);
+            this.tbInputNumber.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(38, 582);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(484, 161);
+            this.groupBox3.TabIndex = 25;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(30, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(428, 84);
+            this.listBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 781);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupEquation);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Newton-Rhapson";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupEquation.ResumeLayout(false);
+            this.groupEquation.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupEquation;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInputNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Newton-Rhapson/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Newton_Rhapson
+{
+    public partial class Form1 : Form
+    {
+        int matrixSize;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                matrixSize = int.Parse(tbInputNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Masukkan nilai dengan benar!");
+                return;
+            }
+            tbInputNumber.Text = "";
+
+            label6.Text = matrixSize.ToString();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            //pertanyaan
+            for (int i = 0; i <= matrixSize; i++)
+            {
+                var column = new DataGridViewColumn();
+                column.HeaderText = (i == matrixSize ? "C" : "X^" + (matrixSize - i).ToString());
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                column.Width = 30;
+                dataGridView1.Columns.Add(column);
+
+
+            }
+            dataGridView1.Rows.Add();
+
+            String[] captionDataGrid = {"Enter Guess", "Tolerable Error", "Max Step"};
+
+            for (int i = 0; i < 3; i++)
+            {
+                var col_guess = new DataGridViewColumn();
+                col_guess.HeaderText = captionDataGrid[i];
+                col_guess.CellTemplate = new DataGridViewTextBoxCell();
+                col_guess.Width = 80;
+                dataGridView2.Columns.Add(col_guess);
+            }
+            dataGridView2.Rows.Add();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //get dataGrid2 data
+            double guessNumber = double.Parse((string)dataGridView2.Rows[0].Cells[0].Value);
+            double tolerableError = double.Parse((string)dataGridView2.Rows[0].Cells[1].Value);
+            int maxStep = int.Parse((string)dataGridView2.Rows[0].Cells[2].Value);
+
+            //get polinom function
+            int pwr = matrixSize;
+            double[] func_coef = new double[pwr+1];
+            double[] derr_func_coef = new double[pwr];
+            for (int i = (pwr+1); i > 0; i--)
+            {
+                //Console.Write(i);
+                func_coef[i-1] = double.Parse((string)dataGridView1.Rows[0].Cells[(pwr-i+1)].Value);//int.Parse(Console.ReadLine());
+                Console.Write(func_coef[i-1]);
+                Console.Write("\t");
+
+                if (i != (pwr+1))
+                {
+                    derr_func_coef[i-1] = func_coef[i] * (i);
+                    Console.Write(derr_func_coef[i-1]);
+                }
+                Console.WriteLine();
+            }
+
+
+            newton_rhapson(func_coef, derr_func_coef, guessNumber, tolerableError, maxStep);
+
+
+
+        }
+
+        private double functionVal(double[] mat_coef, double x)
+        {
+            double ret = 0;
+            for(int i = 0; i < mat_coef.GetLength(0); i++){
+                ret += mat_coef[i] * Math.Pow(x, i); 
+            }
+            return ret;
+        }
+
+        private void newton_rhapson(double[] _coefficient, double[] _derrivativeCoefficient, double _guessNumber, double _tolerableError, int _maxStep)
+        {
+            int step = 1;
+            bool convergent = true;
+            while (true)
+            {
+                
+                if(functionVal(_derrivativeCoefficient, _guessNumber) == 0)
+                {
+                    Console.WriteLine("Divide by Zero");
+                    break;
+                }
+                double newGuessNumber = _guessNumber - (functionVal(_coefficient, _guessNumber) / functionVal(_derrivativeCoefficient, _guessNumber));
+                String list_box_msg = "iteration-" + step.ToString() + ", x = " + _guessNumber.ToString() + " and f(x0) = " + functionVal(_coefficient, _guessNumber).ToString();
+                Console.WriteLine(list_box_msg);
+                listBox1.Items.Add(list_box_msg);
+                _guessNumber = newGuessNumber;
+                step++;
+
+                if (step > _maxStep)
+                {
+                    convergent = false;
+                }
+
+                if(Math.Abs(functionVal(_coefficient, _guessNumber)) < _tolerableError)
+                {
+                    break;
+                }
+
+
+            }
+
+            if (convergent)
+            {
+                Console.WriteLine("\n The Root is {0}", _guessNumber);
+                label1.Text = "The Root is " + _guessNumber;
+            }
+            else
+            {
+                Console.WriteLine("not convergent");
+                label1.Text = "Not Convergent";
+            }
+
+
+        }
+
+
+    }
+}
